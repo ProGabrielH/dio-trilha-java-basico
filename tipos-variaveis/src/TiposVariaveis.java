80837631c9262f4bfc066f4a02178d1ddeabb991
Gabriel Henrique <gabrielh.jrf@gmail.com>
2023-09-28 15:50:56 -0300

    feat: add TiposVariaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.50; // mesmo que o float no Python (existe float em java, mas é mais convencional usar o double )
+        int idade = 22; // atribuição de valor inteiro
+        final String NOME = "Gabriel Henrique"; // "final" torna a variável em constate, sendo assim imutável
+        char sexo = 'M'; // char é usado para representar um único caractere 
+        boolean empregado = false; // valor booleano represantado por true ou false
+        System.out.println("Nome: "+ NOME);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Idade: "+ idade);
+        System.out.println("Está trabalhando: " + empregado);
+        System.out.println("Salário: " + salarioMinimo);
+
+    }
+}
