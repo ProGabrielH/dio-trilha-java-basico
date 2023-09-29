91833af6fee6d40fad75d7d34a612dd7db3a5924
Gabriel Henrique <gabrielh.jrf@gmail.com>
2023-09-29 00:35:20 -0300

    style: removed exception

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,5 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         double salarioMinimo = 2500.50; // mesmo que o float no Python (existe float em java, mas é mais convencional usar o double )
         int idade = 22; // atribuição de valor inteiro
         final String NOME = "Gabriel Henrique"; // "final" torna a variável em constate, sendo assim imutável
