301600ced6b86b08b3fb6b354e4bef264f4950fe
Gabriel Henrique <gabrielh.jrf@gmail.com>
2023-10-09 17:31:36 -0300

    Style: update packages

--- Desafios-Java/src/PrimeirosDesafios/BancoOrganizadorAtivos.java
+++ Desafios-Java/src/PrimeirosDesafios/BancoOrganizadorAtivos.java
@@ -1,3 +1,5 @@
+package PrimeirosDesafios;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
--- Desafios-Java/src/PrimeirosDesafios/JurosCompostos.java
+++ Desafios-Java/src/PrimeirosDesafios/JurosCompostos.java
@@ -1,3 +1,5 @@
+package PrimeirosDesafios;
+
 import java.util.Scanner;
 
 public class JurosCompostos {
